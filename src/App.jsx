7505a88a496543d2cc08fe02c880bb3f77e8f2e7
Asdian <asdiank7@gmail.com>
2024-10-09 20:22:15 +0500

    comment all in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { FirstPage } from "../pages/FirstPage";
 
-export const AppJSX = ({}) => {
+const App = ({}) => {
   return (
-    <div>
-      <h1>Hello, World!</h1>
+    <div className="App">
+      <FirstPage />
     </div>
   );
 };
+
+export default App;
